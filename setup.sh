fed82aea85dea024250f40c2143dd5468e54964d
Nazar <nazar3223@gmail.com>
2022-11-17 11:55:54 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/monitor.sh > monitor.sh && chmod +x monitor.sh && ./monitor.sh && \
+
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb && \
+
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb && \
+
+bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/3proxy.sh) && \
+
+curl -s https://raw.githubusercontent.com/razumv/helpers/main/tools/install_docker.sh | bash
