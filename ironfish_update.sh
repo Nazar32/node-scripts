ba4f2f207d7e07c61a1a54e5245c7af1b8f371a2
Nazar <nazar3223@gmail.com>
2023-01-20 12:22:38 +0200

    Update ironfish_update.sh

--- ironfish_update.sh
+++ ironfish_update.sh
@@ -1,4 +1,4 @@
 docker-compose down && \
 docker-compose pull && \
-№docker-compose run --rm --entrypoint "./bin/run migrations:start" ironfish && \
+# docker-compose run --rm --entrypoint "./bin/run migrations:start" ironfish && \
 docker-compose up -d
