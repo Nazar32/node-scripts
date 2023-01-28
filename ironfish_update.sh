5deb4c6c65053abc1a2f26892ea3fcc55de5c93e
Nazar <nazar3223@gmail.com>
2023-01-28 08:54:30 +0200

    Update ironfish_update.sh

--- ironfish_update.sh
+++ ironfish_update.sh
@@ -1,4 +1,5 @@
 docker-compose down && \
 docker-compose pull && \
-# docker-compose run --rm --entrypoint "./bin/run migrations:start" ironfish && \
+wget -O $HOME/.ironfish/hosts.json https://raw.githubusercontent.com/DOUBLE-TOP/guides/main/ironfish/hosts.json && \
+docker-compose run --rm --entrypoint "./bin/run migrations:start" ironfish && \
 docker-compose up -d
