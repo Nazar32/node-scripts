e7184989ee3283b0cba4f11c68ebb15665d0fdef
Nazar <nazar3223@gmail.com>
2023-03-02 07:50:33 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 curl -s https://raw.githubusercontent.com/razumv/helpers/main/monitor.sh > monitor.sh && chmod +x monitor.sh && ./monitor.sh && \
 
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb && \
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb && \
 
-sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb && \
+sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb && \
 
 bash <(curl -s https://raw.githubusercontent.com/DOUBLE-TOP/tools/main/3proxy.sh) && \
 
